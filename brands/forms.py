cb0982339a7861c69e82e18e5231e16f451336a1
João Bruno M. Fernandes <jbrunomf@outlook.com>
2025-02-04 20:54:37 -0400

    Add CRUD views and templates for Brand model
    Renamed the `name` field to `brand_name` in the Brand model for clarity and updated its ordering. Implemented ListView and CreateView with corresponding templates to enable basic CRUD functionality. Enhanced forms, updated URLs, and adjusted sidebar navigation for seamless integration.

--- brands/forms.py
+++ brands/forms.py
@@ -6,4 +6,9 @@ from brands import models
 class BrandForm(forms.ModelForm):
     class Meta:
         model = models.Brand
-        fields = ['brand_name', 'description']
+        fields = ["brand_name", "description", ]
+        labels = {"brand_name": "Nome", "description": "Descrição"}
+        widgets = {
+            "brand_name": forms.TextInput(attrs={"class": "form-control"}),
+            "description": forms.Textarea(attrs={"class": "form-control", 'rows': 5}),
+        }
